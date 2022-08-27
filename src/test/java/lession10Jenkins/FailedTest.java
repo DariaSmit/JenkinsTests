7318514d8264d277c47ab352d83f4e14e553ac1b
darya <DariaSmit@yandex.ru>
2022-08-27 11:39:41 +0300

    lession10 rename FailedTest

--- src/test/java/lession10Jenkins/FailedTest.java
+++ src/test/java/lession10Jenkins/FailedTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class NegativeTest {
+public class FailedTest {
 
     @Test
     void Test00() {
