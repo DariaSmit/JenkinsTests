b83228116c8c04622282d6448ebb502b8ff3f7b0
darya <DariaSmit@yandex.ru>
2022-08-28 15:57:48 +0300

    lession10 added properties test

--- src/test/java/lession10Jenkins/demoqa/properties/SystemPropertiesTests.java
+++ src/test/java/lession10Jenkins/demoqa/properties/SystemPropertiesTests.java
@@ -3,43 +3,47 @@ package lession10Jenkins.demoqa.properties;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+@Tag("systemProperties")
 public class SystemPropertiesTests {
 
-    @Test
-    void test1() {
-        String browser = System.getProperty("browser");
-        System.out.println(browser);
-    }
+        @Test
+        void test1() {
+            String browser = System.getProperty("browser");
+            System.out.println(browser);
+        }
 
-    @Test
-    void test2() {
-        System.setProperty("browser", "chrome");
-        String browser = System.getProperty("browser");
-        System.out.println(browser);
+        @Test
+        void test2() {
+            System.setProperty("browser", "chrome");
+            String browser = System.getProperty("browser");
+            System.out.println(browser);
 
-    }
-    @Test
-    void test3() {
+        }
 
-        String browser = System.getProperty("browser", "opera");
-        System.out.println(browser);
-    }
-    @Test
-    void test4 () {
-        System.setProperty("browser", "chrome");
-        String browser = System.getProperty("browser", "opera");
-        System.out.println(browser);
-    }
-    @Test
-    @Tag("test5")
-    void test5 () {
-        String browser = System.getProperty("browser", "chrome");
-        String version = System.getProperty("version", "100");
-        String browserSize = System.getProperty("browserSize", "1920x1080");
+        @Test
+        void test3() {
 
-        System.out.println(browser);
-        System.out.println(version);
-        System.out.println(browserSize);
+            String browser = System.getProperty("browser", "opera");
+            System.out.println(browser);
+        }
+
+        @Test
+        void test4() {
+            System.setProperty("browser", "chrome");
+            String browser = System.getProperty("browser", "opera");
+            System.out.println(browser);
+        }
+
+        @Test
+        @Tag("test5")
+        void test5() {
+            String browser = System.getProperty("browser", "chrome");
+            String version = System.getProperty("version", "100");
+            String browserSize = System.getProperty("browserSize", "1920x1080");
+
+            System.out.println(browser);
+            System.out.println(version);
+            System.out.println(browserSize);
          /*
            gradle clean test5
                 chrome
@@ -50,6 +54,6 @@ public class SystemPropertiesTests {
                 99
                 300x300
          */
-    }
+        }
 
-}
+    }
